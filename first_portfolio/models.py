5f8b997627a5202d052f017f1dab01427b08eb43
Miu <miu@Mius-MacBook-Pro.local>
2021-03-05 14:28:49 -0800

    update image_fiels

--- first_portfolio/models.py
+++ first_portfolio/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from stdimage import StdImageField
 
 # Create your models here.    
+
+
 class Work(models.Model):
 
   title = models.CharField(max_length=250)
-  image = models.ImageField(upload_to='img/upload', null= True ,blank=True)
+  # image = models.ImageField(upload_to='img/upload', null= True ,blank=True)
+  image = StdImageField(upload_to='img/upload', null= True ,blank=True)
+
   url = models.URLField(blank=True)
   date = models.TextField(max_length=50)
   partner = models.CharField(max_length=250)
